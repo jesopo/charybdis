b00435853c94e52a9eb864b07f098ef27bd9df93
jesopo <github@lolnerd.net>
2020-07-19 08:40:06 +0000

    add IsConfSecure to show_iline_prefix

--- ircd/hostmask.c
+++ ircd/hostmask.c
@@ -694,6 +694,8 @@ show_iline_prefix(struct Client *sptr, struct ConfItem *aconf, char *name)
 		*prefix_ptr++ = '+';
 	if(IsConfDoSpoofIp(aconf))
 		*prefix_ptr++ = '=';
+	if(IsConfSecure(aconf))
+		*prefix_ptr++ = '%';
 	if(IsOper(sptr) && IsConfExemptFlood(aconf))
 		*prefix_ptr++ = '|';
 	if(IsOper(sptr) && IsConfExemptDNSBL(aconf) && !IsConfExemptKline(aconf))
